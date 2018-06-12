1ee40543fae95c385d8be7029e67e16b0ee445fc
Shreyans Shrivastava <sshrivastava@yodlee.com>
2018-06-12 11:42:04 +0530

    Updated Utils

--- src/utils/extra.js
+++ src/utils/extra.js
@@ -2,9 +2,8 @@ export const getRandomInt = (min, max) => (Math.floor(Math.random() * (max - min
 
 export const sleep = (time) => (new Promise((resolve) => setTimeout(resolve, time)))
 
-export const sortByField = (field) => {
-    var reverse = false;
-    var primer = function (a) { return a.toUpperCase() };
+export const sortByField = (field, reverse, primer) => {
+
     var key = primer ?
         function (x) { return primer(x[field]) } :
         function (x) { return x[field] };
@@ -12,6 +11,7 @@ export const sortByField = (field) => {
     reverse = !reverse ? 1 : -1;
 
     return function (a, b) {
-        return [a = key(a), b = key(b), reverse * ((a > b) - (b > a))];
+        return a = key(a), b = key(b), reverse * ((a > b) - (b > a));
     }
 }
+
